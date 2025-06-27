ca6dc5fb16f6d9c4f5e4281c8dab8de57e55c7b8
Prabha Adhitya <prabhaadhitya01@gmail.com>
2025-06-27 16:19:29 +0530

    Updated login logic and fixed navbar bug

--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -1,16 +1,9 @@
 import Loader from './Loader';
 import ProductItem from './ProductItem';
-// import { useState } from 'react';
 
 function ProductList(props) {
 
-  // const [count, setCount] = useState(0);
-
   const {filteredSortedGroupByArr} = props;
-
-  // function handleCount() {
-
-  // }
   
   return (
     <div>
